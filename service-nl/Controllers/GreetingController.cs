a478811d76db157ad0a42e916325a5653d85c695
Erik van Appeldoorn <erik@nforza.nl>
2021-10-18 11:53:08 +0200

    added instructions for .net 5 services

---
+++ service-nl/Controllers/GreetingController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace service_nl.Controllers
+{
+    [ApiController]
+    [Route("greeting")]
+    public class GreetingController : ControllerBase
+    {
+        private readonly ILogger<GreetingController> _logger;
+
+        public GreetingController(ILogger<GreetingController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Greet()
+        {
+            return "Hallo, iedereen van ITUMX !";
+        }
+    }
+}
